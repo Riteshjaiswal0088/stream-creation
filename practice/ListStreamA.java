d19181c7984f4a6cbfc2f511e73fa990598db4dd
ritesh <reepu.jaiswal@gmail.com>
2022-12-29 18:23:21 +0530

    unCommented code
    * uncommented code both are applicable

--- practice/ListStreamA.java
+++ practice/ListStreamA.java
@@ -6,12 +6,11 @@ import java.util.stream.IntStream;
 public class ListStreamA {
     public static void main(String[] args) {
         var list = new ArrayList<>();
-   /*     for (int i = 20; i > 0; i--) {
+        for (int i = 20; i > 0; i--) {
             list.add(i);
-        }*/
-      // list.forEach(System.out::println);
-        IntStream.range(1, 20)
-                .forEach(list::add);
+        }
+        list.forEach(System.out::println);
+        IntStream.range(1, 20).forEach(list::add);
         System.out.println(list);
 
     }
